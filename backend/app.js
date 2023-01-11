340fc56d8f5bd4252fa74ccfe34aa6e3454cf3e8
ianMuchesia <ianmuchesia@yahoo.com>
2023-01-11 17:25:37 +0300

    final wednesday

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 const tasks = require("./routes/tasks")
+const connectDB = require("./db/connect")
 const port = 3000
-
+require('dotenv').config()
 //middleware
 app.use(express.json())
 
@@ -10,4 +11,16 @@ app.use(express.json())
 app.use("/api/v1/tasks", tasks)
 
 
-app.listen(port, console.log(`server running on port ${port}`))
+
+//start server
+const start = async()=>{
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, console.log(`server running on port ${port}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
