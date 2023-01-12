5dd8bfcc0d5eb5ae707ec95ef3462919b362bf33
ianMuchesia <ianmuchesia@yahoo.com>
2023-01-12 12:57:55 +0300

    fetchesData

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 const tasks = require("./routes/tasks")
 const connectDB = require("./db/connect")
+var cors = require('cors')
 const port = 3000
 require('dotenv').config()
 //middleware
 app.use(express.json())
-
+app.use(cors())
 //routes
 app.use("/api/v1/tasks", tasks)
 
